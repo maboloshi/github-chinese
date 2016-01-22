12f81bed42e65561071b9bf4b60c95f18c765718
楼教主 <cik520@qq.com>
2016-01-22 13:08:21 +0800

    更新翻译

--- locals.js
+++ locals.js
@@ -5,8 +5,9 @@ __GitHub_I18N['zh'] = {
 	// 通用
 	"Search GitHub": "GitHub 一下！",
 	"Pull requests": "合并请求",
-	"Issues": " 问题",
+	"Issues": " 问题 ",
 	"Gist": "代码片段",
+	"This repository": "当前仓库",
 
 	"You have no unread notifications": "您没有未读通知",
 	"Create new…": "新建…",
@@ -14,6 +15,7 @@ __GitHub_I18N['zh'] = {
 
 	"New repository": " 新建仓库",
 	"New organization": " 新建组织",
+	"New issue": " 新建问题",
 
 	"Signed in as": "您好 ",
 	"Your profile": "您的主页",
@@ -23,6 +25,14 @@ __GitHub_I18N['zh'] = {
 	"Help": "帮助",
 	"Settings": "设置",
 	"Sign out": "退出",
+
+	// 未登录状态
+	"Sign up": " 注册 ",
+	"Sign in": " 登录 ",
+	"Features": "特点",
+	"Enterprise": "创业",
+	"Pricing": "价格",
+
 
 
 	// 主页
@@ -38,19 +48,34 @@ __GitHub_I18N['zh'] = {
 
 	
 	// 个人首页
+	"Follow": " 关注",
+	"Change your avatar": "修改头像",
 	"Followers": "粉丝",
 	"Starred": "赞了",
 	"Following": "关注",
+	"Organizations": "组织",
 	"Contributions": " 贡献",
 	"Repositories": " 仓库",
 	"Public activity": " 动态",
-	"Edit profile": "修改设置",
+	"Edit profile": " 修改设置",
 	"Popular repositories": "流行的仓库",
 	"Repositories contributed to": "您贡献过的仓库",
 	"Contribution activity": "近期贡献信息",
 	"Search": "搜索",
 	"New": "新建",
 	"Mirrors": "镜像",
+	"Public contributions": " 他的贡献",
+
+	"Summary of pull requests, issues opened, and commits.": "包括 合并请求, 提问, 提交.",
+	"Learn how we count contributions": " 你想知道如何计算贡献的吗",
+	"Less": "少",
+	"More": "多",
+	"Contributions in the last year": "过去一年的贡献",
+	"Longest streak": "连续贡献最长天数",
+	"Current streak": "当前连续贡献天数",
+
+	"Pull Requests": " 合并请求",
+	"Issues reported": " 问题报告",
 
 
 	// 仓库页
@@ -60,17 +85,36 @@ __GitHub_I18N['zh'] = {
 	"Watch": " 关注",
 	"Star": " 点赞",
 	"Fork": " 收藏",
-	"Pull Request": "合并请求",
+	"Unwatch": " 取消关注",
+	"Unstar": " 取消点赞",
+	"Pull Request": " 合并请求 ",
 
 	"commits": "次提交",
+	"branch": "个分支",
 	"branches": "个分支",
+	"release": "次发布",
 	"releases": "次发布",
 	"contributor": "个贡献者",
+	"contributors": "个贡献者",
 	"Copy to clipboard": "复制到剪切板",
+
+	"Branch:": "分支:",
+	"Switch branches/tags": "选择分支或标签",
+	"Branches": " 分支 ",
+	"Tags": " 标签 ",
+	"Nothing to show": "暂无",
 
 	"New pull request": "发起合并请求",
 	"New file": "新建文件",
 	"Find file": "查找文件",
 	"Download ZIP": "下载 ZIP",
+
+	"Edit": "修改",
+	"Save": "保存",
+	"Cancel": "取消",
+	"Description": "描述",
+	"Website": "网址",
+	"Short description of this repository": "简短的描述下这个仓库",
+	"Website for this repository (optional)": "这个仓库的演示网址 (可不填)",
 	
 };
