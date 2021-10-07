b6a9b63e68097e9c1e3241166cbf6bee6390de72
maboloshi <7850715+maboloshi@users.noreply.github.com>
2021-10-07 13:35:41 +0800

    暂且删除未使用的`时间节点翻译`函数和`贡献日历`函数

--- main.js
+++ main.js
@@ -19,15 +19,10 @@
 
     var lang = 'zh'; // 中文
 
-    // 2016-04-18 github 将 jquery 以 amd 加载，不暴露到全局了。
-    // var $ = require('github/jquery')['default'];
-
     // 要翻译的页面
     var page = getPage();
 
     transTitle(); // 页面标题翻译
-    // timeElement(); // 时间节点翻译
-    // setTimeout(contributions, 100); // 贡献日历翻译 (日历是内嵌或ajax的, 所以基于回调事件处理)
     walk(document.body); // 立即翻译页面
     watchUpdate();
 
@@ -270,93 +265,6 @@
         }
 
         return false; // 没有翻译条目
-    }
-
-
-    /**
-     * 时间节点翻译
-     */
-    function timeElement() {
-        if (!window.RelativeTimeElement) { // 防止报错
-            return;
-        }
-
-        var RelativeTimeElement$getFormattedDate = RelativeTimeElement.prototype.getFormattedDate;
-        var TimeAgoElement$getFormattedDate = TimeAgoElement.prototype.getFormattedDate;
-        // var LocalTimeElement$getFormattedDate = LocalTimeElement.prototype.getFormattedDate;
-
-        var RelativeTime = function (str, el) { // 相对时间解析
-            if (/^on ([\w ]+)$/.test(str)) {
-                return '于 ' + el.title.replace(/ .+$/, '');
-            }
-
-            // 使用字典公共翻译的第二个正则翻译相对时间
-            var time_ago = I18N[lang].pubilc.regexp[1];
-            return str.replace(time_ago[0], time_ago[1]);
-        };
-
-        RelativeTimeElement.prototype.getFormattedDate = function () {
-            var str = RelativeTimeElement$getFormattedDate.call(this);
-            return RelativeTime(str, this);
-        };
-
-        TimeAgoElement.prototype.getFormattedDate = function () {
-            var str = TimeAgoElement$getFormattedDate.call(this);
-            return RelativeTime(str, this);
-        };
-
-        LocalTimeElement.prototype.getFormattedDate = function () {
-            return this.title.replace(/ .+$/, '');
-        };
-
-        // 遍历 time 元素进行翻译
-        // 2016-04-16 github 改版，不再用 time 标签了。
-        var times = document.querySelectorAll('time, relative-time, time-ago, local-time');
-        Array.prototype.forEach.call(times, function (el) {
-            if (el.getFormattedDate) { // 跳过未注册的 time 元素
-                el.textContent = el.getFormattedDate();
-            }
-        });
-    }
-
-
-    /**
-     * 贡献日历 基于事件翻译
-     */
-    function contributions() {
-        var tip = document.getElementsByClassName('svg-tip-one-line');
-
-        // 等待 IncludeFragmentElement 元素加载完毕后绑定事件
-        // var observe = require('github/observe').observe;
-
-        define('github/hans-contributions', ['./observe'], function (observe) {
-            observe(".js-calendar-graph-svg", function () {
-                setTimeout(function () { // 延时绑定 mouseover 事件，否则没法翻译
-                    var $calendar = $('.js-calendar-graph');
-                    walk($calendar[0]); // 翻译日历部分
-
-                    $calendar.on('mouseover', '.day', function () {
-                        if (tip.length === 0) { // 没有 tip 元素时退出防止报错
-                            return true;
-                        }
-
-                        var data = $(this).data(); // 获取节点上的 data
-                        var $tip = $(tip[0]);
-
-                        $tip.html(data.count + ' 次贡献 ' + data.date);
-
-                        var rect = this.getBoundingClientRect(); // 获取元素位置
-                        var left = rect.left + window.pageXOffset - tip[0].offsetWidth / 2 + 5.5;
-
-                        $tip.css('left', left);
-                    });
-                }, 999);
-            });
-        });
-
-        ghImport('github/hans-contributions')['catch'](function(e) {
-            setTimeout(function() { throw e });
-        });
     }
 
 })(window, document);
