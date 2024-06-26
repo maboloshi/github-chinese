6bbe249165a9caab7f700d820fe14a964610bd72
TC999 <3177754310@qq.com>
2024-06-26 18:24:44 +0800

    去除多余更改

--- main(greasyfork).user.js
+++ main(greasyfork).user.js
@@ -4,7 +4,7 @@
 // @description  中文化 GitHub 界面的部分菜单及内容。原作者为楼教主(http://www.52cik.com/)。
 // @copyright    2021, 沙漠之子 (https://maboloshi.github.io/Blog)
 // @icon         https://github.githubassets.com/pinned-octocat.svg
-// @version      1.9.2-2024-06-09
+// @version      1.9.2-2024-06-26
 // @author       沙漠之子
 // @license      GPL-3.0
 // @match        https://github.com/*
