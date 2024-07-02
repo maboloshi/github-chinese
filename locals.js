b0e8d5f5cf10eb9a61f54453c9f631bcd8b6c28a
TC999 <3177754310@qq.com>
2024-07-02 18:29:39 +0800

    注释

--- locals.js
+++ locals.js
@@ -9012,8 +9012,8 @@ I18N.zh["repository/discussions"] = { // 讨论页面
             "This user is the author of this discussion.": "该用户是此讨论的作者。",
 
         // 开始讨论按钮上方小字
-            "I have done a": "我已",
-            "Reminder to": "记得",
+            "I have done a": "我已", // 二选一展示
+            "Reminder to": "记得", // 同上
             "search for similar discussions": "搜索相似话题先",
     },
     "regexp": [ // 正则翻译
