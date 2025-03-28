5f4de090c0e742da78f2a42c92d0445a25e3df90
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-03-28 12:35:40 +0800

    账户设置 - 个人资料 - 私人贡献显示

--- locals.js
+++ locals.js
@@ -3191,6 +3191,7 @@ I18N["zh-CN"]["settings/profile"] = { // 设置 - 个人资料
                 // 顶部提醒
                 "Profile updated successfully —": "个人资料更新成功 —",
                 "view your profile.": "查看您的个人资料。",
+                "Visitors will now see your public and anonymized private contributions.": "现在访客可以看到您的公开和匿名私人贡献。",
 
             "Trending settings": "趋势设置",
             "Preferred spoken language": "首选语言",
