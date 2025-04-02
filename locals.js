c63b0dd9045f4dc8fcaab2bdd303f08b582ccf55
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-04-02 13:53:55 +0800

    仓库 - 议题、拉取请求杂项

--- locals.js
+++ locals.js
@@ -8136,6 +8136,7 @@ I18N["zh-CN"]["repository/pull_issue_public"] = { // 仓库 - 议题和拉取请
                     "will be able to comment on this pull request once more.": "将能够再次对这个拉取请求发表评论。",
                     "You can always lock this pull request again in the future.": "您今后仍可以随时再次锁定此拉取请求。",
                     "You can always lock this issue again in the future.": "您今后仍可以随时再次锁定此议题。",
+                    "Everyone will be able to comment on this issue once more. You can always lock this issue again in the future.": "任何人将能够再次对这个议题发表评论。您今后仍可以随时再次锁定此议题。",
 
                 // 隐藏评论对话框
                 "The reason will be displayed to describe this comment to others.": "将显示原因，以便向其他人描述此评论。",
@@ -8456,8 +8457,10 @@ I18N["zh-CN"]["repository/issues"] = { // 仓库 - 议题页面
             "added a parent issue": "添加了一个父议题",
             "added a commit that references this issue": "添加了一个引用此议题的提交",
             "transferred this issue from": "将此议题转移，从",
-            "as off topic": "因偏离主题",
-            "as too heated": "因争论不休",
+            "as off topic": "为偏离主题",
+            "as too heated": "为争论不休",
+            "as spam": "为垃圾信息",
+            "as resolved": "为已解决",
             "added a sub-issue": "添加了一个子议题",
             "removed a parent issue": "移除了一个父议题",
             "removed a sub-issue": "移除了一个子议题",
@@ -8999,6 +9002,7 @@ I18N["zh-CN"]["repository/pull"] = { // 仓库 - 某个拉取请求页面
             "pushed a commit that referenced this pull request": "推送了一个引用此拉取请求的提交",
             "suggested changes": "建议更改",
             "deleted the branch": "删除了分支",
+            "deleted a comment from": "删除了评论从",
 
             // 隐藏
             "Load more…": "加载更多…",
@@ -9099,6 +9103,8 @@ I18N["zh-CN"]["repository/pull"] = { // 仓库 - 某个拉取请求页面
             "reopened this": "已重新打开",
             "closed this by deleting the head repository": "已关闭因仓库已删除",
             "Requested changes": "请求更改",
+            "restored the": "恢复",
+            "unlocked this conversation": "解锁此对话",
 
             //
             "This branch has not been deployed": "该分支尚未部署",
@@ -9624,6 +9630,8 @@ I18N["zh-CN"]["repository/pull"] = { // 仓库 - 某个拉取请求页面
 
         "Repository owner locked as": "仓库所有者锁定为",
             "resolved": "已解决",
+            "too heated": "争论不休",
+            "off-topic": "偏离主题",
 
     },
     "regexp": [ // 正则翻译
