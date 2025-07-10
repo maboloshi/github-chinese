d271a8ee4f497aec820727ce3765eda9958ac864
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-07-10 13:05:52 +0800

    新版仓库创建页

--- locals.js
+++ locals.js
@@ -7531,6 +7531,8 @@ I18N["zh-CN"]["page-new-repo"] = {// 仓库 - 新建/导入/复刻仓库
                 "Already have a project repository elsewhere?": "在其他地方已有仓库？",
                 "Import a repository": "导入仓库",
                 "Required fields are marked with an asterisk (*).": "带星号 (*) 的为必填项。",
+            
+            "🎉 Try the new experience": "🎉 尝试新版页面",
 
             "Repository template": "仓库模板",
                 "Fetching templates…": "正在获取模板…",
@@ -7698,6 +7700,43 @@ I18N["zh-CN"]["page-new-repo"] = {// 仓库 - 新建/导入/复刻仓库
             "You are creating a fork in your personal account.": "您正在向您的个人账户中创建一个复刻。",
             "Create fork": "创建复刻",
 
+        // 新版新建仓库页
+            "Switch back to classic experience": "回到经典页面",
+            "Repositories contain a project's files and version history.": "仓库包含项目的文件和版本历史。",
+            "Have a project elsewhere?": "在其他地方已有仓库？",
+
+            "General": "通常",
+                "Great repository names are short and memorable. How about": "好的仓库名称应该简单且容易记忆。这个怎么样：",
+
+            "Configuration": "配置",
+                "Choose visibility": "选择可见性",
+                    "Choose who can see and commit to this repository": "选择谁可以查看并提交此仓库",
+                "Start with a template": "使用模板开始",
+                    "Templates pre-configure your repository with files.": "模板会用文件预先配置您的仓库。",
+                "Add README": "添加自述文件",
+                    "READMEs can be used as longer descriptions.": "自述文件可以用作更长的描述。",
+                    "About READMEs": "关于自述文件",
+                // gitignore
+                    ".gitignore tells git which files not to track.": ".gitignore 告诉 git 不追踪哪些文件。",
+                    "About ignoring files": "关于忽略文件",
+
+                    "Choose a .gitignore template": "选择 .gitignore 模板",
+                        "Search templates": "搜索模板",
+                        "No .gitignore": "无",
+                "Add license": "添加许可证",
+                    "Licenses explain how others can use your code.": "许可证解释其他人如何使用您的代码。",
+                    "About licenses": "关于许可证",
+
+                    "Search licenses": "选择许可证",
+                    "No license": "无",
+                "Connect GitHub Apps": "连接 GitHub 应用",
+                    "Connect this repository to apps": "将此仓库连接到", 
+                    "is subscribed to": "订阅的应用",   
+
+                    "Choose GitHub Apps": "选择 GitHub 应用",
+                    "Search apps": "搜索应用",
+                    "Cancel and close": "关闭",
+
     },
     "regexp": [ // 正则翻译
         // [/([^ ]+) is available\./,"$1 名称可用。"],
@@ -7707,6 +7746,9 @@ I18N["zh-CN"]["page-new-repo"] = {// 仓库 - 新建/导入/复刻仓库
         // [/\(repository already exists\)/, "（仓库已存在）"], // 复刻页面
         [/You are creating a fork in the ([^ ]+) organization./, "您正在 $1 组织中创建一个复刻。"], // 复刻页面
         [/Updating branches and (\d+) commit authors?…/, "更新分支和 $1 个提交者…"], // 仓库导入第二页
+        //[/Connect this repository to apps ([^ ]+) is subscribed to/, "将此仓库连接到 $1 订阅的应用程序"],
+        [/Auto-installed by ([^ ]+)/, "由 $1 自动安装"],
+        [/(\d+) apps? selected/, "已选择 $1 个应用"],
         ...I18N["zh-CN"]["repository-public"]["regexp"],
         // [/, and (\d+) more/, "，以及其他 $1 个组织"], // 用户 浮动信息卡
         // [/(\d+) repositor(y|ies)/, "$1 个仓库"], // 组织  浮动信息卡
