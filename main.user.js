e0c6154d582946c027afc69c3cb90ebd3381c91c
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-07-14 10:32:32 +0800

    功能：检测词库文件是否加载

--- main.user.js
+++ main.user.js
@@ -623,6 +623,12 @@
      * init 函数：初始化翻译功能。
      */
     function init() {
+        if (typeof I18N === 'undefined') {
+            alert('依赖文件 locals.js 未加载，脚本无法运行！');
+        // 也可以选择 return 或 throw new Error
+        } else {
+            console.log(`依赖文件 locals.js 已加载`);
+        }
         // 设置中文环境
         document.documentElement.lang = CONFIG.LANG;
 
