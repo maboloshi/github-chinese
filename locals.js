e982344d17bf50cfe41cc8db214c5c702ac0007c
tangyuan0821 <tangyuan0821@hotmail.com>
2025-07-20 13:11:19 +0800

    一些细微之处的更新

--- locals.js
+++ locals.js
@@ -724,6 +724,7 @@ I18N["zh-CN"]["public"] = { // 公共区域翻译
             "Try Enterprise": "试用企业版",
                 "Free": "免费",
             "Feature preview": "功能预览",
+            "GitHub MCP Server":"GitHub MCP 服务器",
                 "New": "新",
                 // 对话框
                 "Feature preview dialog": "功能预览对话框",
@@ -5554,7 +5555,7 @@ I18N["zh-CN"]["settings/security_analysis"] = { // 设置 - 代码安全性与�
                             // [/Security settings updated for ([^ ]+)\'s repositories./, "更新了 $1 的仓库的安全设置。"],
 
             // 仓库
-                "Security and analysis features help keep your repositories secure and updated.": "安全和分析功能可帮助您确保仓库的安全和更新。",
+                "Advanced Security features help keep your repository secure and updated. By enabling these features, you're granting us permission to perform read-only analysis on your repository.": "安全和分析功能有助于确保您的仓库安全和更新。通过启用这些功能，您授予我们对您的仓库执行只读分析的权限。",
 
             "Disable all": "禁用全部",
             "Enable all": "启用全部",
@@ -18711,6 +18712,8 @@ I18N["zh-CN"]["notifications"] = { // 通知页面
         "Notifications you save will appear here to read later.": "您保存的通知会出现在这里，以便以后阅读。",
         "Mark notifications as done so you can move on with your work.": "将通知标记为已完成，以便您可以继续工作。",
         "New activity appears in your inbox.": "新活动出现在您的收件箱中。",
+        "All done here!":"这里都完成了！",
+        "You're caught up on this filter.":"你已处理完当前筛选下的所有任务。",
 
         // /notifications?query=repo 某个仓库
         "We've noticed that you rarely interact with this repository, are you sure you need notifications?": "我们注意到您很少与此仓库交互，您确定需要通知吗？",
@@ -18973,6 +18976,8 @@ I18N["zh-CN"]["issues"] = { // 议题页面
         "all of GitHub": "所有 GitHub",
         "or try an": "或者尝试",
         "advanced search": "高级搜索",
+        "No results":"无结果",
+        "Try adjusting your search filters.":"尝试调整您的搜索筛选条件。",
 
         // "Use the links above to find what you’re looking for, or try": "使用上面的链接找到您要找的内容，或尝试",
         // "a new search query": "新的搜索查询",
@@ -19022,6 +19027,7 @@ I18N["zh-CN"]["issues"] = { // 议题页面
         [/(\d+) Closed/, "$1 已关闭"],
         [/(\d+) tasks? done/, "$1 个任务完成"],
         [/(\d+) of (\d+) tasks?/, "$1 / $2 个任务"],
+        [/(\d+) results?/, "$1 个结果"],
         [/(\d+) tasks?/, "$1 个任务"],
         [/(\d+) review approvals?/, "$1 次审查批准"],// 拉取请求页 "已批准' 浮动提示
         [/(\d+) review requesting changes?/, "$1 条请求更改评论"],
@@ -19029,8 +19035,9 @@ I18N["zh-CN"]["issues"] = { // 议题页面
         [/([\d,]+) linked pull requests?/, "$1 个关联拉取请求"],
         [/(\d+) \/ (\d+) checks? OK/, "$1 / $2 检查 OK"], // 对勾 的提醒 /pulls
         [/Assigned to ([^ ]+)/, "分配给 $1"],
-        // [/Updated/, "更新于"],
-        [/#(\d+) opened/, "#$1 打开于"],
+        [/Created by ([^ ]+)/, "由 $1 创建"],
+        [/Updated/, "更新于"],
+        [/#([^ ]+) opened/, "#$1 打开于"],
         [/#(\d+) by/, "#$1 打开者"],
         [/Notify someone on an issue with a mention, like: @([^ ]+)./, "在某个问题上通知并提及某人，例如：@$1。"], // 专业提示
     ],
