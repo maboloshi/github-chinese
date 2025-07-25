a4ae8728a274f0ae3be754186a4e3328ffe68f9f
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-07-25 19:10:07 +0800

    修复

--- locals.js
+++ locals.js
@@ -13367,8 +13367,8 @@ I18N["zh-CN"]["repository/releases"] = { // 仓库 - 发行版页面
             "This is a draft and won’t be seen by the public unless it’s published.": "这是一个草案，除非发布，否则不会被公众看到。",
             "Discard draft": "丢弃草案",
 
-            //"New release": "新建发行版",
-            "New Release": "新建发行版",
+            "New Release": "新建发行版", // 上方
+            "New release": "新建发行版", // 下方
 
             "Release notes": "发行版说明",
                 "Select a previous tag to create generated release notes": "选择一个先前的标签以生成发行说明",
