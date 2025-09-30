3be31a936018d6997b90aeac0ef53029d1ce6337
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-09-30 10:01:04 +0800

    设置 - 高级请求分析

--- locals.js
+++ locals.js
@@ -3074,6 +3074,7 @@ I18N["zh-CN"]["settings-menu"] = { // 设置 - 公共部分
         "Billing and licensing": "账单和许可",
             "New": "新",
             "Usage": "使用情况",
+            "Premium request analytics": "高级请求分析",
             "Budgets and alerts": "预算和警报",
             "Licensing": "许可",
             "Payment information": "支付信息",
@@ -3883,6 +3884,23 @@ I18N["zh-CN"]["settings/billing"] = { // 设置 - 账单和计划
                 "Price/unit": "单价",
                 "Billed amount": "计费",
 
+        // 高级请求分析 https://github.com/settings/billing/premium_requests_usage
+            "Usage analytics for premium requests in your personal account.": "在您的个人账户中针对高级请求的使用分析。",
+
+            "Billed premium requests": "计费高级请求",
+                "Increase your budget": "提高您的预算",
+                    "to use premium requests beyond your included request limit.": "以便在超出包含请求额度后继续使用。",
+
+            "Included premium requests consumed": "包含高级请求",
+                "Premium requests included in your": "高级请求包含在您的",
+                    "Copilot plan": "Copilot 计划中",
+                    // 后续走正则
+
+            // 用量分析
+                "Model": "模型",
+                    "Included requests": "包含请求",
+                    "Billed requests": "计费请求",
+
         // 预算和警报 https://github.com/settings/billing/budgets
            "Account budgets": "账户预算",
                 "New budget": "新建",
@@ -4329,9 +4347,17 @@ I18N["zh-CN"]["settings/billing"] = { // 设置 - 账单和计划
 
     },
     "regexp": [ // 正则翻译
+        // 高级请求分析（词条打架调整位置） https://github.com/settings/billing/premium_requests_usage
+        [/. Monthly limit resets in (\d+) days? on (.+)./, "。将在 $1 天后（$2）重置。"],
+        [/Usage for (.+) - (.+). Price per premium request is \$0.04./, (match, p1, p2) => {
+            const dateRegExp = I18N["zh-CN"]["public"]["time-regexp"];
+            const translatedP1 = dateRegExp.reduce((acc, [pattern, replacement]) => acc.replace(pattern, replacement), p1);
+            const translatedP2 = dateRegExp.reduce((acc, [pattern, replacement]) => acc.replace(pattern, replacement), p2);
+            return `${translatedP1}-${translatedP2}用量。高级请求价格为 $0.04 / 个。`;
+        }],
 
         // billing 概况页面
-        [/(?:Gross metered usage|Included usage discounts) for (.+) - (.+)./, (match, p1, p2) => { // 概况下方小字，过于啰嗦直接省略
+        [/(?:Gross metered usage|Included usage discounts) for (.+) - (.+).$/, (match, p1, p2) => { // 概况下方小字，过于啰嗦直接省略
             //const translatedP1 = I18N["zh-CN"]["public"]["time-regexp"][p1] || p1;
             //const translatedP2 = I18N["zh-CN"]["public"]["time-regexp"][p2] || p2;
             const dateRegExp = I18N["zh-CN"]["public"]["time-regexp"];
@@ -4450,9 +4476,9 @@ I18N["zh-CN"]["settings/billing"] = { // 设置 - 账单和计划
         //     return `${translatedDate}` + optKey[opt];
         // }],
 
-        // 计费用量 https://github.com/settings/billing/usage
-            [/^Group: (None|Product|SKU|Repository)$/, function(all, group) {
-                var groupKey = {'None': '无','Product': '产品','SKU': 'SKU','Repository': '仓库'};
+        // 计费用量 https://github.com/settings/billing/usage + 高级请求分析 https://github.com/settings/billing/premium_requests_usage
+            [/^Group(?: by)?: (None|Product|SKU|Repository|Models)$/, function(all, group) {
+                var groupKey = {'None': '无','Product': '产品','SKU': 'SKU','Repository': '仓库', 'Models': '模型'};
                 return '分组：' + groupKey[group];
             }],
             [/Usage for (.+)./, (match, p1) => {
