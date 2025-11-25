d2edb1636fcda0788f4d68692e4818ba6e9c0257
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2025-11-25 18:48:50 +0800

    改进 `GPG删除对话框` 翻译并移到正确位置
    Co-authored-by: maboloshi <7850715+maboloshi@users.noreply.github.com>

--- locals.js
+++ locals.js
@@ -5292,14 +5292,14 @@ I18N["zh-CN"]["settings/keys"] = { // 设置 - SSH 与 GPG 公钥
             "This action": "该操作",
             "cannot": "不能",
             "be undone. This will permanently delete the SSH key and if you’d like to use it in the future, you will need to upload it again.": "被撤销。这将永久地删除 SSH 密钥，如果您想在未来使用它，您将需要再次上传它。",
-            "Commits you signed with this key may become unverified after removing it.": "删除后，您使用此密钥签名的提交可能会变成未验证。",
-            "Learn more about persistent commit signature verification.": "了解更多关于持久提交签名验证的信息。",
             "I understand, delete this SSH key": "我明白了，删除此 SSH 密钥",
 
             // GPG 密钥删除 对话框
             "Are you sure you want to delete this GPG key?": "您确定要删除此 GPG 密钥吗？",
             "be undone. This will permanently delete the GPG key, and if you’d like to use it in the future, you will need to upload it again.": "被撤销。这将永久地删除 GPG 密钥，如果您想在未来使用它，您将需要再次上传它。",
             "Any commits you signed with this key will become unverified after removing it.": "删除后，您使用此密钥签名的任何提交都将变成未验证。",
+            "Commits you signed with this key may become unverified after removing it.": "移除此密钥后，您使用它签署的提交可能会变为未验证状态。",
+            "Learn more about persistent commit signature verification.": "了解更多关于持久性提交签名验证的信息。",
             "I understand, delete this GPG key": "我明白了，删除此 GPG 密钥",
             "Okay, you have successfully deleted that key.": "好的，您已成功删除该密钥。",
 
