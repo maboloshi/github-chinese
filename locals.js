2a7ed6b15da6c3ff1549021e77fe06315419294e
PtJade Ceramic <185668489+PtJade-Ceramic@users.noreply.github.com>
2026-03-22 02:21:17 +0800

    Update Chinese translation for GitHub Actions permissions
    Signed-off-by: PtJade Ceramic <185668489+PtJade-Ceramic@users.noreply.github.com>

--- locals.js
+++ locals.js
@@ -25075,7 +25075,7 @@ I18N["zh-CN"]["orgs/settings/actions"] = { // 组织设置 - 操作
                         "Workflows have read and write permissions in the repository for all scopes.": "工作流程在仓库中对所有作用域具有读和写的权限。",
                     "Read repository contents and packages permissions": "读取仓库的内容和软件包的权限",
                         "Workflows have read permissions in the repository for the contents and packages scopes only.": "工作流程在仓库中仅对内容和软件包作用域具有只读的权限。",
-                    "Choose whether GitHub Actions can create pull requests or submit approving pull request reviews.": "选择 GitHub Actions 能否创建拉取请求或提交对拉取请求的审查批准。",
+                    "Choose whether GitHub Actions can create pull requests or submit approving pull request reviews.": "选择 GitHub Actions 能否创建拉取请求，或发表审查意见批准拉取请求。",
                     "Allow GitHub Actions to create and approve pull requests": "允许 GitHub Actions 创建和批准拉取请求",
                         "This controls whether GitHub Actions can create pull requests or submit approving pull request reviews.": "这控制 GitHub Actions 能否创建拉取请求或提交对拉取请求的审查批准。",
                     // 顶部提醒
