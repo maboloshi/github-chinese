8cc79fb5ee776488dc3cee99cbc6bdd1874e9a01
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2026-04-08 10:55:19 +0800

    删除多余词条

--- locals.js
+++ locals.js
@@ -8155,7 +8155,6 @@ I18N["zh-CN"]["repository-public"] = { // 仓库 - 公共部分
             "Actions": "操作",
             "Projects": "项目",
             "Models": "模型",
-            "Security": "安全",
             "Security and quality": "安全和质量",
             "Insights": "洞察",
             "Settings": "设置",
