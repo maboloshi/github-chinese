dcb525ce9a72b015d61244d981f7282e655d8b25
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2026-05-04 22:44:36 +0800

    仓库 - 拉取请求

--- locals.js
+++ locals.js
@@ -11260,6 +11260,9 @@ I18N["zh-CN"]["repository/pull"] = { // 仓库 - 某个拉取请求页面
                 "command line": "命令行",
                 "to resolve conflicts.": "来解决冲突。",
                 "These conflicts are too complex to resolve in the web editor": "这些冲突过于复杂，无法在网页编辑器中解决",
+
+                "Edit on the web": "在线编辑",
+                "Fix with Copilot": "使用 Copilot 修复",
             "Conflicting files": "冲突的文件:",
 
             "Require approval from specific reviewers before merging": "合并前需要特定审查者的批准",
